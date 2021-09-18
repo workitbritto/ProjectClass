1c0b5263decc41c9aed648d3b07bcdcfc496aa7a
arulprasad5 <arulprasad1997@gmail.com>
2021-09-18 20:44:17 +0530

    Update Code from Merin

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -14,6 +14,20 @@ private void method3() {
 	System.out.println("m3");
 
 }
+private void method4() {
+	System.out.println("m4");
+
+}
+private void method5() {
+	System.out.println("m5");
+
+}
+private void method6() {
+	System.out.println("m6");
+
+}
+
+
 }
 
 	
