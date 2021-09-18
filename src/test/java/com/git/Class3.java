60382741246e19bc6c4766a6d289d516cd042693
Anishkumar1999 <anishsankar1999@gmail.com>
2021-09-18 16:53:10 +0530

    Anish update code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,5 +1,14 @@
 package com.git;
 
 public class Class3 {
-
+private void t1() {
+	System.out.println("a");
 }
+private void t2() {
+	System.out.println("b");
+}
+private void t3() {
+System.out.println("c");
+}
+}
+
