7e941f6313ffb42d3f896ff5780e2684403de2f3
Pavithrathara <pavithrathara1@gmail.com>
2021-09-18 21:13:53 +0530

    Methods added

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,4 +2,10 @@ package com.git;
 
 public class Class3 {
 
+	private void Javaclass() {
+		System.out.println("Javaclass");
+	}
+	private void SeleniumClass() {
+		System.out.println("SeleniumClass");
+	}
 }
