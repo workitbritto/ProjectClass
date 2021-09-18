d458062f1d0186ab68b79c21bbdfd9c6d165c4fd
joynabi <joynabiya.arava398@gmail.com>
2021-09-18 15:11:22 +0530

    task done

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,4 +2,17 @@ package com.git;
 
 public class Class3 {
 
-}
+	private void tc1() {
+		System.out.println("test 1");
+
+	}
+	private void tc2() {
+		System.out.println("test 2");
+
+	}
+	private void tc3() {
+		System.out.println("test 3");
+
+	}
+	}
+
