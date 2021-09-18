ae9e8dbc79b41e2ce4f9f62d4ad79ddd685cfde2
jeevajana200 <jeevajana200@gmail.com>
2021-09-18 16:15:09 +0530

    pushed by jeeva

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,6 +2,29 @@ package com.git;
 
 public class Class3 {
 	
+	private void rx100() {
+		
+		System.out.println("RX100");
+
+	}
+	
+	private void rx135() {
+		
+		System.out.println("RX135");
+
+	}
+	
+	private void ns200() {
+		
+		System.out.println("NS200");
+
+	}
+	
+	private void rs200() {
+		
+		System.out.println("RS200");
+
+	}
 
 
 }
