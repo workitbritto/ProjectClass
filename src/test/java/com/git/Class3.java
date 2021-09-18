acd100eb3ea03701fc012e041ef7bd797bcef400
RajiDinesh <d.rajeswari.work@gmail.com>
2021-09-18 16:06:16 +0530

    Rajeswari updated code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,4 +2,15 @@ package com.git;
 
 public class Class3 {
 
+	public void login() {
+		System.out.println("login");
+	}
+
+	public void searchPage() {
+		System.out.println("Search");
+	}
+
+	public void bookAPage() {
+		System.out.println("confirm");
+	}
 }
