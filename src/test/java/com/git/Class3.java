a4ebb31c1ca9ad8adbb99bdccd29c4443b16efe5
alfaiz-92 <mohamedalfaiz29@gmail.com>
2021-09-18 15:53:26 +0530

    Updated Code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,7 +2,52 @@ package com.git;
 
 public class Class3 {
 	
+	private void method1() {
+		System.out.println("method-1");
 
+	}
+
+	private void method2() {
+		System.out.println("method-2");
+
+	}
+
+	private void method3() {
+		System.out.println("method-3");
+
+	}
+
+	private void method4() {
+		System.out.println("method-4");
+
+	}
+
+	private void method5() {
+		System.out.println("method-5");
+
+	}
+
+	private void method6() {
+		System.out.println("method-6");
+
+	}
+
+	private void method7() {
+		System.out.println("method-7");
+
+	}
+	public static void main(String[] args) {
+		
+	Class3 cls =new Class3();
+	cls.method1();
+	cls.method2();
+	cls.method3();
+	cls.method4();
+	cls.method5();
+	cls.method6();
+	cls.method7();
+	
+	}
 
 }
 
