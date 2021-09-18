dd1068564ac2afceaf21293023f5e8a6db06d4a8
maheuma14 <umaa9310@gmail.com>
2021-09-18 15:48:31 +0530

    updated class

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,5 +1,17 @@
 package com.git;
 
 public class Class3 {
+	private void company() {
+		System.out.println("name");
+
+	}
+	private void employee() {
+	    System.out.println("id");
+
+	}
+	private void customers() {
+		System.out.println("code");
+
+	}
 
 }
