138ff9f607fe1e259c0cb78a97ce5bc297199f12
Diya1308 <esathya04@gmail.com>
2021-09-18 15:26:00 +0530

    Sathya Updated Code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,7 +2,18 @@ package com.git;
 
 public class Class3 {
 	
+private void sky() {
+	System.out.println("Sky");
 
+}
+private void moon() {
+	System.out.println("Moon");
+
+}
+private void star() {
+	System.out.println("Star");
+
+}
 
 }
 
