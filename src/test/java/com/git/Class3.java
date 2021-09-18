bd12ea08eb7fbd3a5da2419238bae21f20a8a867
usha4420 <usha4420@gmail.com>
2021-09-18 15:47:13 +0530

    Usha updated code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,7 +2,14 @@ package com.git;
 
 public class Class3 {
 	
-
-
+private void Employee() {
+System.out.println("Employee");
+}
+private void Company() {
+System.out.println("Company");
+}
+private void Salary() {
+System.out.println("Salary");
+}
 }
 
