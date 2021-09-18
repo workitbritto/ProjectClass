98c830ab834c0556283b6eaedb6fef0ea84cbd8b
HariniThirumoorthy <harinee1995@gmail.com>
2021-09-18 15:53:47 +0530

    Updated the methods

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,5 +1,15 @@
 package com.git;
 
 public class Class3 {
+	
+	private void pullRequest() {
+		System.out.println("PULL REQUEST");
+	}
+	private void pushRequest() {
+		System.out.println("PUSH REQUEST");
+	}
+	private void cloneRequest() {
+		System.out.println("CLONE REQUEST");
+	}
 
 }
