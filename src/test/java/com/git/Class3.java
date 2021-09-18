b2065700dc7e824da60e77155cf91a1174166af7
kathiravanmanokaran <kathirmanokaran@gmail.com>
2021-09-18 17:00:03 +0530

    kathir updated code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,7 +1,17 @@
 package com.git;
 
 public class Class3 {
-	
+	private void sunday() {
+		System.out.println("sun");}
+
+
+		private void monday() {
+			System.out.println("mon");}
+		
+		
+			private void tuesday() {
+				System.out.println("tue");
+	}
 
 
 }
