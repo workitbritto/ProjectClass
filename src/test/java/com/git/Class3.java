1a1a02fb7fde6f33470927fa95051eb0e94ad66d
Rajadurai07 <rajadurai07121994@gmail.com>
2021-09-18 20:13:47 +0530

    rajadurai updated code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,4 +2,16 @@ package com.git;
 
 public class Class3 {
 
+	private void java() {
+		System.out.println("java");
+
+	}
+	private void framework() {
+		System.out.println("framework");
+	}
+	
+	private void project() {
+		System.out.println("project");
+
+	}
 }
