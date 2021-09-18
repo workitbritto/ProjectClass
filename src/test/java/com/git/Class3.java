d7c9a1c05a3d598b712095a849fedbf42c9d1687
SheebaSelvaraj <sheebarajkumar92@gmail.com>
2021-09-18 17:52:37 +0530

    sheeba updated

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -2,7 +2,14 @@ package com.git;
 
 public class Class3 {
 	
-
-
+	private void mm1() {
+		System.out.println("1");
+}
+private void mm2() {
+	System.out.println("2");
+}	
+private void mm3() {
+	System.out.println("3");
+}	
 }
 
