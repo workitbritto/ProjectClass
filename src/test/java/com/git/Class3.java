7321fee0d8c27ae849a78cacadc2398084a58b10
yagesh1 <90631165+yagesh1@users.noreply.github.com>
2021-09-18 18:42:18 +0530

    Revert "Anish update code"

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,21 +1,8 @@
 package com.git;
 
 public class Class3 {
-<<<<<<< HEAD
-private void t1() {
-	System.out.println("a");
-}
-private void t2() {
-	System.out.println("b");
-}
-private void t3() {
-System.out.println("c");
-}
-}
-=======
 	
 
 
 }
->>>>>>> f599dd46735dc530da3b9e842f105aac59185ef3
 
