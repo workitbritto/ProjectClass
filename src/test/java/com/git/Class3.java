917219e0bb855cb7a8bc57f18eb5ff2d847123bf
Jayaprakassh <jprakassh831680@gmail.com>
2021-09-18 11:26:29 +0530

    updated code

--- src/test/java/com/git/Class3.java
+++ src/test/java/com/git/Class3.java
@@ -1,5 +1,15 @@
 package com.git;
 
 public class Class3 {
+	
+	private void test1() {
+		System.out.println("ok");
+	}
+	private void test2() {
+		System.out.println("ok");
+	}
+	private void test3() {
+		System.out.println("ok");
 
+	}
 }
